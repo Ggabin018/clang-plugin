85ae2b017c8384b7fbf136a2734d14b7cc85af1c
Gabin Clerbout <gabin.clerbout@epita.fr>
2024-11-02 11:42:08 +0100

    add .clang-tidy and inside sh

--- clang-tidy.sh
+++ clang-tidy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-
-echo clang-tidy "$@" -load ~/clang-plugin/libEpitaClangTidyPlugin.so -- -I"."
-clang-tidy "$@" -load ~/clang-plugin/build/libEpitaClangTidyPlugin.so -- -I"."
+usr=`whoami`
+echo clang-tidy "$@" --config-file="/home/$usr/clang-plugin/.clang-tidy" -load ~/clang-plugin/libEpitaClangTidyPlugin.so -- -I"."
+clang-tidy "$@" --config-file ~/clang-plugin/.clang-tidy  --load ~/clang-plugin/build/libEpitaClangTidyPlugin.so -- -I"."
